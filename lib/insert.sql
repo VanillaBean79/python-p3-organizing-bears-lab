604c5937dcdc332e69d48a277dc5b2d73c768205
Ruben Salazar <rubensalazar@rocketmail.com>
2025-01-31 10:39:56 -0600

    bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'brown', 'calm', true),
+  ('Rowdy', 3, 'M', 'black', 'aggressive', true),
+  ('Tabitha', 7, 'F', 'white', 'gentle', true),
+  ('Sergeant Brown', 10, 'M', 'brown', 'gruff', true),
+  ('Melissa', 2, 'F', 'golden', 'friendly', true),
+  ('Grinch', 6, 'M', 'green', 'mischievous', false),
+  ('Wendy', 4, 'F', 'gray', 'curious', true),
+  (NULL, 1, 'M', 'orange', 'playful', true);
